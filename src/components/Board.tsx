4a1fce9c7547bfb8ad0ed7a83226d1d4b63e12e6
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-03-18 15:43:19 +0200

    Added Board component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,23 @@
 import Button from './Button.tsx'
+import TaskCard, { type TaskCardProps } from './TaskCard.tsx'
+
+
+const tasks: TaskCardProps[] = [
+    { title: "Web Design", description: "Wireframing, mockups, client collaboration" },
+    { title: "Mobile App", description: "Wireframing, mockups, client collaboration" },
+    { title: "Dashboard", description: "Wireframing, mockups, client collaboration" },
+    { title: "App Development", description: "Wireframing, mockups, client collaboration" },
+    { title: "Dashboard", description: "Wireframing, mockups, client collaboration" },
+    { title: "Landing Page", description: "Wireframing, mockups, client collaboration" },
+    { title: "Mobile App", description: "Wireframing, mockups, client collaboration" },
+    { title: "Web Development", description: "Wireframing, mockups, client collaboration" },
+    { title: "App Development", description: "Wireframing, mockups, client collaboration" }
+]
 
 const Board = () => {
     return (
         <div
-            className="bg-white fixed top-[12vh] left-35 border border-gray-300 rounded-3xl w-[67%] h-[83vh]"
+            className="bg-white fixed top-[12vh] left-35 border border-gray-300 rounded-3xl w-[67%] h-[85vh] pb-2"
         >
             <div
                 className="mt-3 px-6 flex items-center justify-between"
@@ -14,6 +28,19 @@ const Board = () => {
                     Tasks
                 </h1>
                 <Button text="New Task" />
+            </div>
+            <div
+                className="h-[88%] mt-3 w-[96%] mx-auto grid grid-cols-3 grid-rows-3 gap-3 place-items-center"
+            >
+                {tasks.map(task => {
+                return (
+                    <TaskCard 
+                        key={task.title}
+                        title={task.title}
+                        description={task.description}
+                    />
+                )
+            })}
             </div>
         </div>
     )
---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,47 @@
+
+export interface TaskCardProps {
+    title: string
+    description: string
+}
+
+interface TagProps {
+    name: string
+    type: TagType
+}
+
+type TagType = "#0000FF" | "#e703f8" | "#fc0398" | "#ff8000"
+
+const TaskCard = ({title, description}: TaskCardProps) => {
+    const tagTypes: TagType[] = ["#ff8000", "#0000FF","#fc0398", "#e703f8"]
+    const randomColor = tagTypes[title.length % tagTypes.length]
+
+    return (
+        <div
+            className="border border-gray-300 rounded-xl px-6 py-4 h-full w-full shadow-md"
+        >
+            <Tag
+                name={title}
+                type={randomColor}
+            />
+            <p>
+                {description}
+            </p>
+        </div>
+    )
+}
+
+const Tag = ({name, type}: TagProps) => {
+    return (
+        <div
+            className={`w-fit py-[4px] px-4 rounded-3xl mb-3 font-bold`}
+            style={{
+                color: type,
+                backgroundColor: `${type}1A`
+            }}
+        >
+            {name}
+        </div>
+    )
+}
+
+export default TaskCard
