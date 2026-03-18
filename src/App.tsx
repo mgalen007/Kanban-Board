4b9e69f607e23891e58ec7afb618b1987f8be742
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-03-18 13:30:48 +0200

    Refactored SideBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react'
 import SideBar from './components/SideBar.tsx'
 
 const App = () => {
+  const [activeIcon, setActiveIcon] = useState('home')
+
   return (
     <div>
-      <SideBar active="home" />
+      <SideBar active={activeIcon} onIconClick={setActiveIcon} />
     </div>
   )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,54 +1,50 @@
+import { type ReactNode } from 'react'
 import { House, UserRound, Settings, Presentation, LogOut } from 'lucide-react'
 
 interface SideBarProps {
-    active: "home" | "profile" | "settings"
+    active: string
+    onIconClick: (name: string) => void
 }
 
-const SideBar = ({ active }: SideBarProps) => {
+interface Icon {
+    name: string
+    node: ReactNode
+}
+
+const SideBar = ({ active, onIconClick }: SideBarProps) => {
+
+    const icons: Icon[] = [
+        { name: 'logo', node: <Presentation size={50} color="#FFF" strokeWidth={1} /> },
+        { name: 'home', node: <House size={30} color="#FFF" strokeWidth={1} /> },
+        { name: 'profile', node: <UserRound size={30} color="#FFF" strokeWidth={1} /> },
+        { name: 'settings', node: <Settings size={30} color="#FFF" strokeWidth={1} /> },
+        { name: 'logout', node: <LogOut size={30} color="#FFF" strokeWidth={1} /> }
+    ]
+
+    let optionIcons = [icons[1], icons[2], icons[3]]
+
     return (
         <nav className="bg-[#1e0058] border w-fit fixed left-0 h-full px-5 flex flex-col justify-between py-5 items-center">
             <div>
-                <Presentation size={50} color="#FFF" strokeWidth={1} />
+                {icons[0].node}
             </div>
-            <div 
-                className="h-[25%] flex flex-col justify-between w-fit"
+            <div
+                className="h-[30%] flex flex-col justify-between items-center"
             >
-                <div
-                    className={`p-3 rounded-[50%] ${(active == "home" && "bg-[#734edc]")}`}
-                >
-                    <House 
-                        size={30}
-                        color="#FFF"
-                        strokeWidth={1}
-                    />
-                </div>
-                <div
-                    className={`p-3 rounded-[50%] ${(active == "profile" && "bg-[#734edc]")}`}
-                >
-                    <UserRound
-                        size={30} 
-                        color="#FFF" 
-                        strokeWidth={1} 
-                    />
-                </div>
-                <div
-                    className={`p-3 rounded-[50%] ${(active == "settings" && "bg-[#734edc]")}`}
-                >
-                    <Settings 
-                        size={30} 
-                        color="#FFF" 
-                        strokeWidth={1} 
-                    />
-                </div>
+                {optionIcons.map(icon => {
+                    return (
+                        <div
+                            key={icon.name}
+                            className={`${active == icon.name && "p-3 rounded-[50%] bg-[#724ddc]"}`}
+                            onClick={() => onIconClick(icon.name)}
+                        >
+                            {icon.node}
+                        </div>
+                    )
+                })}
             </div>
-            <div 
-                className="w-fit"
-            >
-                <LogOut 
-                    size={30} 
-                    color="#FFF" 
-                    strokeWidth={1} 
-                />
+            <div>
+                {icons[4].node}
             </div>
         </nav>
     )
