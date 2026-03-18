0e93b77eb82a6b21c3bff45301551c1360781bc9
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-03-18 14:12:30 +0200

    Added SearchBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
+import React from 'react'
 import SideBar from './components/SideBar.tsx'
+import SearchBar from './components/SearchBar.tsx'
 
 const App = () => {
+
   const [activeIcon, setActiveIcon] = useState('home')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+    setSearchQuery(e.target.value)
+  }
 
   return (
     <div>
-      <SideBar active={activeIcon} onIconClick={setActiveIcon} />
+      <SideBar 
+        active={activeIcon} 
+        onIconClick={setActiveIcon} 
+      />
+      <SearchBar 
+        placeholder="Search" 
+        onChange={handleSearch} 
+        value={searchQuery} 
+      />
     </div>
   )
 }
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,31 @@
+import { Search } from 'lucide-react'
+import React from 'react'
+
+interface SearchBarProps {
+    placeholder: string
+    onChange: React.ChangeEventHandler<HTMLInputElement>
+    value: string
+}
+
+function SearchBar({ placeholder, onChange, value }: SearchBarProps) {
+    return (
+        <div
+            className="fixed left-40 top-5 w-[20%]"
+        >
+            <Search
+                color="gray"
+                className="absolute top-2 left-3"
+            />
+            <input 
+                type="text" 
+                placeholder={placeholder} 
+                name="search-bar"
+                value={value}
+                onChange={onChange}
+                className="border border-black/20 bg-white rounded-lg w-full pr-4 py-2 pl-10 focus:border-gray-500 focus:outline-none"
+            ></input>
+        </div>
+    )
+}
+
+export default SearchBar
