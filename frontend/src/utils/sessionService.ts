adfa850d8bf932838101407caa8440e8eeda1094
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-03-21 11:25:09 +0200

    feature(frontend): Added a service to handle session data

---
+++ frontend/src/utils/sessionService.ts
@@ -0,0 +1,31 @@
+import apiFetch from './api.ts'
+
+interface Session {
+    _id: string
+    userID: string
+    tasks: string[]
+    createdAt: string
+    updatedAt: string
+    __v: number
+}
+
+const endpoint = "/sessions"
+const token = ''
+
+export async function createSession(): Promise<Session> {
+    try {
+        const newSession = await apiFetch<Session>(endpoint, token, "POST")
+        return newSession
+    } catch(err) {
+        throw err as Error
+    }
+}
+
+export async function getSession(id: string): Promise<Session> {
+    try {
+        const session = await apiFetch<Session>(`${endpoint}/${id}`, token, "GET")
+        return session
+    } catch(err) {
+        throw err as Error
+    }
+}
